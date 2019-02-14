54a2727f87d909c54d4f547d32604d5ec3e40a68
Tyler Robinson <TJRV@outlook.com>
2019-02-14 06:52:32 +0000

    Fixed bug that allowed empty url or name for website

--- src/main/java/group9rcraggs/application/controller/PageValidator.java
+++ src/main/java/group9rcraggs/application/controller/PageValidator.java
@@ -1,6 +1,7 @@
 package group9rcraggs.application.controller;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import group9rcraggs.application.domain.Page;
@@ -14,6 +15,10 @@ import group9rcraggs.application.domain.Page;
 		@Override
 		public void validate(Object target, Errors errors) {
 			Page p = (Page) target;
+
+			//Rejects any page name or URL that's empty
+			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "", "Field cannot be empty.");
+			ValidationUtils.rejectIfEmptyOrWhitespace(errors, "url", "", "Field cannot be empty.");
 
 			//Rejects any page URL that doesn't begin with http:// or https://
 			if(!p.getUrl().matches("(http://|https://).*")) {
--- src/main/java/group9rcraggs/application/controller/WebsiteValidator.java
+++ src/main/java/group9rcraggs/application/controller/WebsiteValidator.java
@@ -1,6 +1,7 @@
 package group9rcraggs.application.controller;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import group9rcraggs.application.domain.Website;
@@ -16,6 +17,11 @@ public class WebsiteValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		Website w = (Website) target;
 
+		//Rejects any page name or URL that's empty
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "", "Field cannot be empty.");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "url", "", "Field cannot be empty.");
+		
+		//Rejects any website URL that doesn't begin with http:// or https://
 		if(!w.getUrl().matches("(http://|https://).*")) {
 			errors.rejectValue("url", "", "URL must begin with http:// or https://");
 		}
