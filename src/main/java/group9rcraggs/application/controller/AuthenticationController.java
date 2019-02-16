ed89447b36849230cfe334cca80ada56576eb8d0
Mantas Paukstaitis <mantas.pau@gmail.com>
2019-02-16 17:09:14 +0000

    fixed checkstyle

--- src/main/java/group9rcraggs/application/controller/AuthenticationController.java
+++ src/main/java/group9rcraggs/application/controller/AuthenticationController.java
@@ -2,14 +2,13 @@ package group9rcraggs.application.controller;
 
 import java.security.Principal;
 
-import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
+
+
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import group9rcraggs.application.domain.Role;
 import group9rcraggs.application.domain.User;
-import group9rcraggs.application.domain.Website;
 import group9rcraggs.application.repository.UserRepository;
 
 
