3ae2648dedd22c0d4fc7b7af57051179a4df0772
LucasPetka <33698376+LucasPetka@users.noreply.github.com>
2019-02-21 23:12:34 +0000

    Checkstyle

--- src/main/java/group9rcraggs/application/controller/ProfileController.java
+++ src/main/java/group9rcraggs/application/controller/ProfileController.java
@@ -5,21 +5,15 @@ import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import group9rcraggs.application.Tracking;
-import group9rcraggs.application.domain.Page;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 import group9rcraggs.application.domain.User;
 import group9rcraggs.application.domain.Website;
 import group9rcraggs.application.repository.UserRepository;
@@ -61,24 +55,6 @@ public class ProfileController {
 		}
 		return "Profile";
     }
-
-
-//	@InitBinder
-//	protected void initBinder(WebDataBinder binder) {
-//		binder.addValidators(new WebsiteValidator());
-//	}
-	
-
-//	///* Returns view CreateWebTrack *///
-//    @RequestMapping(value = "addWebsite", method = RequestMethod.GET)
-//    public String create(Model model) {
-//		model.addAttribute("website", new Website());
-//		return "CreateWebTrack";
-//
-//}
-    
-  ///* Adds website to database when add is clicked and calls addWebsite *///
-
-    
+  
     
 }
